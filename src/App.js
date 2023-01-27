fdfbe5fa4c9ea8795c90e52ac7e85b5f2275b45f
luvgameon <luvgameon@gmail.com>
2023-01-28 00:54:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  a='Luv'
+  render() {
+    return (
+      <div>This is My Comaponent Based Class {this.a}</div>
+    )
+  }
+}
